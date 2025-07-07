d855532e4346f84c4daff86f0551b5a0ab2d443a
Purga-codes <purgas-world@proton.me>
2025-07-08 00:17:51 +0300

    made a repo + completed day1

---
+++ day1_task.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+print("The name of your band could be " + city + " " + pet)
